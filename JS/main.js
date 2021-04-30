2ec6c2733f75cc3d99e6cd307b120a750aefbb11
CraftyYardBird <61528847+alexdesignpage@users.noreply.github.com>
2021-04-29 23:55:24 -0400

    Week 2

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,42 @@ const links = [
     {
         label: "WEEK2",
         url: "WEEK2/index.html"
+    },
+    {
+        label: "WEEK3",
+        url: "WEEK3/index.html"   },
+    {
+        label: "WEEK4",
+        url: "WEEK4/index.html"
+    },
+    {
+        label: "WEEK5",
+        url: "WEEK5/index.html"
+    },
+    {
+        label: "WEEK6",
+        url: "WEEK6/index.html"   },
+    {
+        label: "WEEK7",
+        url: "WEEK7/index.html"
+    } ,
+    
+    {
+        label: "WEEK8",
+        url: "WEEK8/index.html"   },
+    {
+        label: "WEEK9",
+        url: "WEEK9/index.html"},
+    {
+        label: "WEEK10",
+        url: "WEEK10/index.html"   },
+    {
+        label: "WEEK11",
+        url: "WEEK11/index.html"
+    },
+    {
+        label: "WEEK12",
+        url: "WEEK12/index.html"
     }
 ];
 
@@ -14,7 +50,7 @@ var contents = document.getElementById("contents");
 function generateContents () {
     links.forEach(function (item, index) {
         console.log(index, item);
-        let listItem = "<li><a href=\"" + item.url + "\" >" + item.label + "</a></li>";
+        let listItem = "<div><a href=\"" + item.url + "\" >" + item.label + "</a></div>";
         console.log(listItem);
         contents.innerHTML += listItem;
     })
